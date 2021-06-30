5bbd71b8c7e2f75b7c850813822782f69208f3aa
lucasgarciabertaina <lucasgarciabertaina@gmail.com>
2021-06-30 17:16:48 -0300

    Bulding a frontBase

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img
+    alt="Vue logo"
+    @click="created"
+    @dblclick="showData"
+    src="./assets/logo.png"
+  />
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      costumers: [],
+    };
+  },
+  methods: {
+    showData() {
+      console.log(this.costumers);
+    },
+  },
+  created() {
+    this.costumers = [
+      {
+        id: 1,
+        name: "Sylvia",
+        userId: "3647321",
+        email: "sylvia123@email.com",
+        profilePhoto: "",
+        timeZone: "Pacific Time",
+        currentSing: "11/05/2020 12:48",
+        active: false,
+      },
+      {
+        id: 2,
+        name: "Robin",
+        userId: "2948922",
+        email: "robin@email.com",
+        profilePhoto: "",
+        timeZone: "Eastern European Time",
+        currentSing: "01/02/2010 11:32",
+        active: false,
+      },
+      {
+        id: 3,
+        name: "Joyce",
+        userId: "8156129",
+        email: "joyce456@email.com",
+        profilePhoto: "",
+        timeZone: "European Central Time",
+        currentSing: "25/11/2018 09:28",
+        active: false,
+      },
+      {
+        id: 4,
+        name: "Ting",
+        userId: "6166563",
+        email: "tingtong@email.com",
+        profilePhoto: "",
+        timeZone: "Solomon Time",
+        currentSing: "12/08/2015 13:10",
+        active: false,
+      },
+    ];
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: rgb(232, 247, 243);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-left: 5%;
+  margin-right: 5%;
   margin-top: 60px;
+  border-radius: 5%;
+  background-color: rgba(255, 255, 255, 0.5);
 }
+/* Colors:
+rgb(232,247,243) FONDO */
 </style>
--- src/components/EditDelete.vue
+++
@@ -1,9 +0,0 @@
-<template> 
-    <button class="button"> Edit </button>
-    
-</template>
-<style>
-.button {
-    color: blue
-}
-</style>
