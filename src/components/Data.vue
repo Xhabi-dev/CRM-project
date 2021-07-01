fb64a21b370f582fd76dd7e06826f9adad8a2192
lucasgarciabertaina <lucasgarciabertaina@gmail.com>
2021-07-01 14:36:54 -0300

    Costumers component

---
+++ src/components/Data.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>UserId</p>
+    <h2>3647321</h2>
+    <p>Email</p>
+    <h2>sylvia123@email.com</h2>
+    <p>Timezone</p>
+    <h2>Pacific Time</h2>
+    <p>Current Sing in At</p>
+    <h2>11/05/2020 12:48</h2>
+  </div>
+</template>
+
+<style scoped>
+div {
+  float: right;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+p {
+  margin-top: 10%;
+  font-size: medium;
+}
+h2 {
+  font-weight: 600;
+  margin: 0px;
+  font-size: larger;
+}
+</style>
